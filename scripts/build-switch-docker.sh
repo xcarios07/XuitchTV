a9e648845f4bdf9029a781e44343c1657a4fbf8a
xcarios07 <carreras.andres94@gmail.com>
2026-08-31 03:05:54 -0300

    XuitchTV v0.5.3 OpenGL stability

--- scripts/build-switch-docker.sh
+++ scripts/build-switch-docker.sh
@@ -22,49 +22,29 @@ docker run --rm \
     echo "=== Updating devkitPro packages ==="
     dkp-pacman -Syu --noconfirm
 
-    echo "=== Installing base Switch dependencies ==="
+    echo "=== Installing official Switch dependencies ==="
     dkp-pacman -S --needed --noconfirm \
       switch-dev \
       switch-glfw \
       switch-libwebp \
-      switch-curl
-
-    echo "=== Installing deko3d MPV stack ==="
-    cd /tmp
-
-    BASE_URL="https://github.com/xfangfang/wiliwili/releases/download/v0.1.0"
-
-    PKGS=(
-      "deko3d-8939ff80f94d061dbc7d107e08b8e3be53e2938b-1-any.pkg.tar.zst"
-      "libuam-f8c9eef01ffe06334d530393d636d69e2b52744b-1-any.pkg.tar.zst"
-      "switch-libass-0.17.1-1-any.pkg.tar.zst"
-      "switch-ffmpeg-7.1-1-any.pkg.tar.zst"
-      "switch-libmpv_deko3d-0.36.0-2-any.pkg.tar.zst"
-    )
-
-    for PKG in "${PKGS[@]}"; do
-      echo "Downloading ${PKG}"
-      curl -fL "${BASE_URL}/${PKG}" -o "${PKG}"
-      dkp-pacman -U --noconfirm "${PKG}"
-    done
-
-    cd /work
+      switch-curl \
+      switch-libmpv
 
     source /opt/devkitpro/switchvars.sh
-
     export PATH=/opt/devkitpro/devkitA64/bin:$PATH
     export PKG_CONFIG_PATH=/opt/devkitpro/portlibs/switch/lib/pkgconfig:${PKG_CONFIG_PATH:-}
 
-    echo "=== Checking deko3d MPV header ==="
-    test -f /opt/devkitpro/portlibs/switch/include/mpv/render_dk3d.h
+    echo "=== Verifying official libmpv/OpenGL stack ==="
+    pkg-config --modversion mpv
+    test -f /opt/devkitpro/portlibs/switch/include/mpv/render_gl.h
 
-    echo "=== Configuring XuitchTV ==="
+    echo "=== Configuring XuitchTV with Borealis OpenGL ==="
     rm -rf build_switch
 
     cmake -S . -B build_switch \
-  -DPLATFORM_SWITCH=ON \
-  -DUSE_DEKO3D=ON \
-  -DCMAKE_BUILD_TYPE=Release
+      -DPLATFORM_SWITCH=ON \
+      -DUSE_DEKO3D=OFF \
+      -DCMAKE_BUILD_TYPE=Release
 
     echo "=== Building XuitchTV.nro ==="
     cmake --build build_switch \
